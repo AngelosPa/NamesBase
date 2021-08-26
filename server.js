bb0ad911e1203dbbb16d3b1a0f3eb7bcab111e99
angelo.p <mrpalaiologos@gmail.com>
2021-08-26 10:35:50 +0200

    basic files

---
+++ server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const http = require("http");
+const app = require("./app");
+// to create a server with a port number 5000
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`now Server is listening on http://localhost:${PORT}`);
+});
